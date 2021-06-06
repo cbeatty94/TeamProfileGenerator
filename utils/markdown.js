51939caf25b7d303b144c5fdaa74add98bf4ef04
Burke <beatty.burke@gmail.com>
2021-06-06 11:53:26 -0400

    added generate manager function

--- utils/markdown.js
+++ utils/markdown.js
@@ -1,5 +1,17 @@
-function generateMarkdown(data) {
+const generateManager = function (manager) {
     return `
-    
+    <div class='col'>
+        <div class='card'>
+            <div class='card-body'>
+                <h5 class='card-title'>Manager</h5>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Name: ${manager.name}</li>
+                <li class="list-group-item">Employee ID: ${manager.id}</li>
+                <li class="list-group-item">Email: <a href = 'mailto: ${manager.email}'>${manager.email}</a> </li>
+                <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
+            </ul>
+        </div>
+    </div>
     `
 }
