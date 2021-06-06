44a1d922609d2507a231e68e201b50a3019b5f05
Burke <beatty.burke@gmail.com>
2021-06-06 12:01:39 -0400

    first attempt at generate html function

--- utils/markdown.js
+++ utils/markdown.js
@@ -15,3 +15,27 @@ const generateManager = function (manager) {
     </div>
     `
 }
+
+const generateHTML = function (data) {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <title> Team Profile Generator </title>
+    </head>
+    <body>
+        <h1>Team Profile Generator</h1>
+        <div class='container'>
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+            ${employeeCards}
+            </div>
+        </div>
+    </body>
+    </html>    
+    `
+}
